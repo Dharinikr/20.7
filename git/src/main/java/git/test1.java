0e49d298fe5d8e0ae3d81c8c95d0cc21a6fd5477
dharini <jayadharini@yahoo.com>
2021-07-20 22:34:59 +0100

    GIT 1st commit

---
+++ git/src/main/java/git/test1.java
@@ -0,0 +1,16 @@
+package git;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://uk.yahoo.com/?p=us");
+	}
+
+}
